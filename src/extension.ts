aaf6cee4e12a3e118655e099990eede5da92bf55
siropkin <ivan.seredkin@gmail.com>
2019-08-01 15:30:07 +0300

    Kirilic support for tags added

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,7 @@
 import * as vscode from "vscode";
-import { newEntry } from './newEntry';
-import { showStats } from './showStats';
-import { markAsDone } from './markAsDone';
-//import { listTags, tagsForCompletion } from './listTags';
+import { newEntry } from './sheets';
+import { showStats } from './statistics';
+import { toggleTask } from './tasks';
 import { filterByTags, tagsForIntelliSense } from './tags';
 import { setup } from './settings';
 
@@ -16,7 +15,7 @@ export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(newStatDisposable);
     
     // Mark task as done
-    let newMarkAsDoneDisposable = vscode.commands.registerCommand('tdm.markAsDone', markAsDone);
+    let newMarkAsDoneDisposable = vscode.commands.registerCommand('tdm.markAsDone', toggleTask);
     context.subscriptions.push(newMarkAsDoneDisposable);
 
     // Filter notes by tags
@@ -24,11 +23,13 @@ export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(listTagsDisposable);
 
 	// Show tags in IntelliSense
-	const triggerCharacters = [".", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "=", "_", "$"];
+	const triggerCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+	triggerCharacters.push(...['я', 'ю', 'ч', 'ш', 'щ', 'ж', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ы', 'ь', 'ъ', 'э']);
+	triggerCharacters.push(...["1", "2", "3", "4", "5", "6", "7", "8", "9"]);
+	triggerCharacters.push(...["=", "_", "$", "."]);
 	let tagProvider = vscode.languages.registerCompletionItemProvider('markdown', {
 			provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
 				let linePrefix = document.lineAt(position).text.substr(0, position.character);
-				//if (!linePrefix.endsWith('console.')) {
             	if (!linePrefix.startsWith('tags:')) {
 					return undefined;
 				}
